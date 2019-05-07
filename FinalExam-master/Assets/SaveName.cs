fdaf34b8de46f97d4bf63329c21d607524c01ee6
Thomashl1 <47928543+Thomashl1@users.noreply.github.com>
2019-05-07 16:50:10 -0500

    Initial commit

---
+++ FinalExam-master/Assets/SaveName.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Save : MonoBehaviour
+{
+
+    public InputField PlayerInput;
+    string Name;
+
+    public void Start()
+    {
+        Name = PlayerPrefs.GetString("PlayerName");
+        PlayerInput.text = Name;
+    }
+
+    public void SaveName()
+    {
+        Name = PlayerInput.text;
+        PlayerPrefs.SetString("PlayerName", Name);
+    }
+
+}
